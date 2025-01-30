48558ffdc5913a839fd32243db0315a22274aefd
Charles K. Neimog <charlesneimog@outlook.com>
2025-01-30 15:56:51 -0300

    update example

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -9,28 +9,27 @@ any.auth()
 # Get Spaces
 spaces = any.get_spaces()
 my_space = spaces[0]
-found_objects = (my_space.search_object("Math"))
-# print(found_objects)
 
-# Create Object
+# Create Object on the first space
 note_type = my_space.get_type("Page")
 new_object = Object()
 new_object.name = "Hello World!"
 new_object.icon = "🐍"
 new_object.description = "This is an object created from Python Api"
+new_object.add_title1("Hello")
+new_object.add_title2("From")
+new_object.add_title3("Python")
+new_object.add_codeblock("print('Hello World!')", language="python")
+new_object.add_bullet("1")
+new_object.add_bullet("2")
+new_object.add_bullet("3")
 
-# Body is a markdown string
-new_object.body = ''' 
-# Hello
-## From 
-### Python 
-
-`code string` 
-**Bold Text**
-_Italic Text_
-
-'''
 
 # Add to my_space
 created_object = my_space.create_object(new_object, note_type)
-created_object.delete()
+# created_object.delete()
+
+
+
+
+
