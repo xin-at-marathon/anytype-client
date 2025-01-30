ae932bf1907f95ffd3ac040752c64c93dce3ec2f
Charles K. Neimog <charlesneimog@outlook.com>
2025-01-30 15:55:33 -0300

    add some helpers for text

--- anypy/object.py
+++ anypy/object.py
@@ -1,11 +1,13 @@
 import requests
+from copy import deepcopy
 
 
 class Object:
     def __init__(self):
         self._headers = ''
-        self._space_id = ''
+        self.space_id = ''
         self.id = id
+        self.source = ''
         self.type = 'Page'
         self.name = ''
         self.icon = ''
@@ -23,8 +25,51 @@ class Object:
 
     def delete(self):
         api_url = "http://localhost:31009/v1"
-        url = f"{api_url}/spaces/{self._space_id}/objects/{self.id}"
+        url = f"{api_url}/spaces/{self.space_id}/objects/{self.id}"
         response = requests.delete(url, headers=self._headers)
         response.raise_for_status()  
 
-        pass
+    def create_child(self, obj, type):
+        api_url = "http://localhost:31009/v1"
+        url = f"{api_url}/spaces/{self.id}/objects"
+        object_data = {
+            "icon": obj.icon,
+            "name": obj.name,
+            "description": obj.description,
+            "body": obj.body,
+            "source": self.id,
+            "template_id": "",
+            "object_type_unique_key": type.unique_key,
+        }
+
+        obj_clone = deepcopy(obj)
+        obj_clone._headers = self._headers
+        obj_clone.space_id = self.id
+        response = requests.post(url, headers=self._headers, json=object_data)
+        response.raise_for_status()  
+        for key, value in response.json()["object"].items():
+            obj_clone.__dict__[key] = value
+        return obj_clone
+
+    def add_title1(self, text):
+        self.body += f"# {text}\n"
+
+    def add_title2(self, text):
+        self.body += f"## {text}\n"
+
+    def add_title3(self, text):
+        self.body += f"### {text}\n"
+
+    def add_codeblock(self, code, language=""):
+        self.body += f"``` {language}\n{code}\n```\n"
+
+    def add_bullet(self, text):
+        self.body += f"- {text}\n"
+
+    def add_checkbox(self, text, checked=False):
+        self.body += f"- [x] {text}\n" if checked else f"- [ ] {text}\n"
+
+
+
+
+
