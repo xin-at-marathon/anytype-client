138c0b15226977b5ad53e6b004eea7040d1099de
Charles K. Neimog <charlesneimog@outlook.com>
2025-02-03 16:48:33 -0300

    remove source

--- examples/source.py
+++
@@ -1,9 +0,0 @@
-from anytype import Anytype
-
-any = Anytype()
-any.auth()
-
-spaces = any.get_spaces()
-my_space = spaces[0]
-page_type = my_space.get_type("Note")
-templates = my_space.get_templates(page_type)
